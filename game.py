059656b1d5a042ccf8d4db77674f3d217cd45a67
AndresMurray <andymurray99@hotmail.com>
2024-03-18 16:21:39 -0300

    agrego niveles de dificultad

--- game.py
+++ game.py
@@ -8,9 +8,27 @@ secret_word = random.choice(words)
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 print("¡Bienvenido al juego de adivinanzas!")
+nivel= int (input("Elija el nivel de dificultad: 1)Fácil. 2)Media. 3)Difícil :  "))
 print('Tiene 5 fallos como máximo hasta adivinar la palabra. ¡Buena suerte!')
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
-word_displayed = "_" * len(secret_word)
+if nivel==3:
+    word_displayed = "_" * len(secret_word)
+elif nivel==2:
+    primera=secret_word[0]
+    ultima=secret_word[-1]
+    lineas=len(secret_word)-2
+    guion="_"*lineas
+    word_displayed =(f"{primera}{guion}{ultima}")
+else:
+    vocales=['a','e','i','o','u']
+    palabra= []
+    for i in secret_word:
+        if i in vocales:
+            palabra.append(i)
+        else:
+            palabra.append("_")
+    word_displayed = "".join(palabra)
+    print('eligio el nivel facil, se muestran las vocales de la palabra:')
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
 max_fallos=0
@@ -43,20 +61,38 @@ while (max_fallos<5):
             print("Perdiste :(")
             print(f"La palabra secreta era: {secret_word}")
             break
-    letters = []
-    for letter in secret_word:
-        if letter in guessed_letters:
-            letters.append(letter)
-        else:
-            letters.append("_")
-    word_displayed = "".join(letters)
+    if nivel == 3:
+        letters = []
+        for letter in secret_word:
+            if letter in guessed_letters:
+                letters.append(letter)
+            else:
+                letters.append("_")
+        word_displayed = "".join(letters)
+    elif nivel ==2:
+        lett2=[]
+        lett2.append(primera)
+        for letter in secret_word[1:-1]:
+             if letter in guessed_letters:
+                lett2.append(letter)
+             else:
+                 lett2.append("_")
+        lett2.append(ultima)
+        word_displayed = "".join(lett2)
+    else:
+        lett3=[]
+        for letter in secret_word:
+            if letter in guessed_letters or letter in vocales:
+                lett3.append(letter)           
+            else:
+                lett3.append("_")
+                
+        word_displayed="".join(lett3) 
+                       
     if word_displayed == secret_word:
         print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
         break
     else:
         print(f"Palabra: {word_displayed}")
         continue
-     
-
-
-
+      
